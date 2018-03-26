9af76e25cc723f532522199eb668d62a99dc4c0e
zitro <bryan@bryanortiz.com>
2018-03-26 15:22:43 -0400

    setup

---
+++ nationaltreasure/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nationaltreasure::Application.config.secret_token = '1fa0fecc74c7a38461b2fae51a53f1c8f6f9a73719b1cefb6f35a3cadf51a4d0db049bfda42628f67c076592ffee6a070cdcae3b4bd648031c5c8d52fb37866e'
