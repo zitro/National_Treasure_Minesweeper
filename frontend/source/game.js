fd8a05d777fbdc0f951ff9bca27d293c4df8958d
zitro <bryan@bryanortiz.com>
2018-03-28 17:00:09 -0400

    melissas amazing functions

--- frontend/source/game.js
+++ frontend/source/game.js
@@ -3,9 +3,13 @@ let startGame = () => {
 	let brick = '<img src="media/brick.png">'
 	let startPosition = document.getElementById("1")
 	let currentPosition = startPosition
+	let keyDownEvents = [37, 38, 39, 40, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105]
 	let nc = ['<img src="media/nc/ncbody.png">', '<img src="media/nc/ncoo.png">', '<img src="media/nc/ncmouth.png">', '<img src="media/nc/nchair.png">', '<img src="media/nc/ncglasses.png">' ,'<img src="media/nc/nc80.png">']
 	let nicCage = nc[Math.floor(Math.random()*nc.length)];
+	let treasuresLeft = document.getElementsByClassName('treasure')
 	let lifeMeter = 100;
+	let code = [];
+
 	let bomb = document.createElement('img')
 	bomb.src='media/temp_bomb.png'
 	bomb.className = 'action'
@@ -57,36 +61,74 @@ let startGame = () => {
 			newDiv.innerHTML = nc[Math.floor(Math.random()*nc.length)];
 			currentPosition = newDiv;
 		}
-		hitItem = (item, points, picture) => {
-			if (currentPosition.className === item){
-				actionTimeImgs(item)
-				if (lifeMeter > 0){
-					lifeMeter += points;
-					currentPosition.className = 'tiles'
-				} else if (lifeMeter < 0) {
-					console.log("GAME OVER")
-					//only works if it is at zero
-				}
-			}
+
+
+	hitItem = (item, points) => {
+	if (currentPosition.className === item){
+		actionTimeImgs(item)
+		if (lifeMeter > 0){
+			lifeMeter += points;
+			currentPosition.className = 'tiles'
+		} else if (lifeMeter < 0) {
+			console.log("GAME OVER")
 		}
-		hitItem("bomb", -20, bomb)
-		hitItem("power-up", 5, powerUp)
-		hitItem("treasure", 50, treasure)
-		console.log(lifeMeter);
 	}
+}
 
 	function actionTimeImgs(item){
 		if (item === "bomb"){
 			let win = window.open('media/bomb.png');
-			setTimeout(function () { win.close();}, 1300);
-		}else if (item === "treasure"){
-			let win = window.open('media/treasure3.png');
 			setTimeout(function () { win.close();}, 1500);
 		}else if(item === "power-up"){
 			let win = window.open('media/powerup.png');
-			setTimeout(function () { win.close();}, 1300);
+			setTimeout(function () { win.close();}, 1500);
 		}
 	}
+
+
+	treasureChest = () => {
+			if(currentPosition.className === 'treasure'){
+				if(treasuresLeft.length === 3){
+					//window opens for constitution treasure
+					let win = window.open('media/treasure1.png');
+					setTimeout(function () { win.close();}, 2000);
+					console.log('constitution')
+					currentPosition.className = 'tiles';
+				} else if (treasuresLeft.length === 2){
+					//window opens for constitution glasses
+					let win = window.open('media/treasure2.png');
+					setTimeout(function () { win.close();}, 2000);
+					console.log('glasses')
+					currentPosition.className = 'tiles';
+				} else if (treasuresLeft.length === 1){
+					console.log('code')
+					codeMaker();
+					//show alert for the code
+					let win = window.open('media/treasure3.png');
+					setTimeout(function () { win.close();}, 2000);
+					currentPosition.className = 'tiles';
+				}
+			}
+		}
+		levelUp = () => {
+			// debugger
+		}
+
+		codeMaker = () => {
+  		let counter = 1
+  		while (counter <= 5){
+    		let index = Math.floor(Math.random() * (keyDownEvents.length -1) + 1);
+    		code.push(keyDownEvents[index]);
+    		counter ++;
+  		}
+		}
+		hitItem("bomb", -20)
+		hitItem("power-up", 5)
+		console.log(lifeMeter);
+		treasureChest()
+		levelUp()
+	}
+
 
 
 }
